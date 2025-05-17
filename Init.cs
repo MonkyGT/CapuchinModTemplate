dc0f4a980263cc5186976d32a744fc6a2f54e411
Ben <Ben@DESKTOP-T4M9DNT>
2025-05-17 18:06:01 -0500

    Add project files.

---
+++ Init.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+using CapuchinTemplate.Patches;
+using UnityEngine;
+using BepInEx.Logging;
+
+namespace CapuchinTemplate
+{
+    [BepInPlugin("yourname.modname", "Mod", "1.0.0")]
+    public class Init : BasePlugin
+    {
+        public Harmony harmonyInstance;
+        public Plugin pluginInstance;
+        public static Init initInstance;
+
+        public override void Load()
+        {
+            harmonyInstance = HarmonyPatcher.Patch("yourname.modname");
+            initInstance = this;
+
+            pluginInstance = new GameObject().AddComponent<Plugin>();
+        }
+
+        public override bool Unload()
+        {
+            if (harmonyInstance != null)
+                HarmonyPatcher.Unpatch(harmonyInstance);
+
+            return true;
+        }
+
+        public void Update() { } // Simular to BaseUnityPlugin, you can
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CapuchinTemplate
+{
+    public class Plugin : MonoBehaviour
+    {
+        public static Plugin instance;
+
+        void Start()
+        {
+            instance = this;
+
+            // Add your startup code here, initialize assets, check configurations, etc.
+            WriteLine("Hello world");
+        }
+
+        public void WriteLine(string text, LogLevel severity = LogLevel.Debug) => Init.initInstance.Log.Log(severity, text);
+    }
+}
